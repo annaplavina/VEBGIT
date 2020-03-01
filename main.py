ae89d7f2b636ff76397adbcf1f795f7bb07427d6
Anna Plavina <plavina.anna@yandex.ru>
2020-03-01 03:52:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("enter two integers")
+a = int(input())
+b = int(input())
+print (" you entered two numbers: a = ", a, ", b = ", b, )
+print("choose the operation:")
+print ("1. addition")
+print ("2. Multiplication")
